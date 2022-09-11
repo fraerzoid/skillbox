6129500907ec85d8f187757b1fc5fbf39d108371
SAZavrazhnov <SZavrazhnov@sportmaster.ru>
2022-09-11 16:32:00 +0300

    Сделал ProductItem и ProductList

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ProductList :products="products"></ProductList>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import products from './data/products';
+import ProductList from './components/ProductList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ProductList,
+  },
+  data() {
+    return {
+      products,
+    };
   },
 };
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
---
+++ src/data/products.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    title: 'Планшет HUAWEI MatePad C5e, 4 ГБ/64 ГБ, Wi-Fi + Cellular, темно-синий',
+    price: 14890,
+    image: 'img/x248_trim.webp',
+  },
+  {
+    title: 'Планшет Apple iPad 2021, 256 ГБ, Wi-Fi, серый космос',
+    price: 34770,
+    image: 'img/AppleTablet.jpg',
+  },
+  {
+    title: 'Планшет Xiaomi Pad 5, RU, 6 ГБ/128 ГБ, Wi-Fi, жемчужный белый',
+    price: 34310,
+    image: 'img/XiaomiPad5Ru.jpg',
+  },
+  {
+    title: 'Планшет HUAWEI MatePad T 10s (2020), 4 ГБ/64 ГБ, Wi-Fi + Cellular, насыщенный синий',
+    price: 15990,
+    image: 'img/HuaweiMatePadT10S.jpg',
+  },
+  {
+    title: 'Планшет HUAWEI MatePad 10.4 2022 (2022), 4 ГБ/64 ГБ, Wi-Fi, серый матовый',
+    price: 18990,
+    image: 'img/HuaweiMatePad10_4_2022.jpg',
+  },
+  {
+    title: 'Планшет Samsung Galaxy Tab A7 10.4 SM-T505 (2020), RU, 3 ГБ/32 ГБ, Wi-Fi + Cellular, темно-серый',
+    price: 19999,
+    image: 'img/SamsungGalaxyTabA710_4.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 360,
+    image: 'img/radio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: 'img/toothbrush.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: 'img/phone.jpg',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: 'img/board.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14960,
+    image: 'img/phone-2.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    image: 'img/phone-3.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    image: 'img/bicycle.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34900,
+    image: 'img/wheels.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    image: 'img/scooter.jpg',
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    image: 'img/ripstik.jpg',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    image: 'img/airpods.jpg',
+  },
+  {
+    title: 'Наушники Sony',
+    price: 30690,
+    image: 'img/headphones.jpg',
+  },
+];
